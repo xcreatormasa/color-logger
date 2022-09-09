c7ad15dc33a7609b6eb10f9f4c85abb9cb11bb1b
masa <xcreatormasa@gmail.com>
2022-09-10 01:28:01 +0900

    Generate UMD

--- rollup.config.js
+++ rollup.config.js
@@ -21,97 +21,41 @@ const banner = `
    */
 `
 
-export default [
-  // CommonJS
-  {
-    input: inputFileName,
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: 'inline',
-        banner,
-        exports: 'default',
-      },
-    ],
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {})],
-    plugins: [
-      pluginTypescript(),
-      pluginCommonjs({
-        extensions: ['.js', '.ts'],
-      }),
-      pluginBabel({
-        babelHelpers: 'bundled',
-        configFile: path.resolve(__dirname, '.babelrc.js'),
-      }),
-      pluginNodeResolve({
-        browser: false,
-      }),
-    ],
-  },
-
-  // ES Module
-  {
-    input: inputFileName,
-    output: [
-      {
-        file: pkg.module,
-        format: 'es',
-        sourcemap: 'inline',
-        banner,
-        exports: 'named',
-      },
-    ],
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {})],
-    plugins: [
-      pluginTypescript(),
-      pluginCommonjs({
-        extensions: ['.js', '.ts'],
-      }),
-      pluginBabel({
-        babelHelpers: 'bundled',
-        configFile: path.resolve(__dirname, '.babelrc.js'),
-      }),
-      pluginNodeResolve({
-        browser: false,
-      }),
-    ],
-  },
-
-  // Browser
-  {
-    input: inputFileName,
-    output: [
-      // Uncompressed
-      {
-        name: browserVar,
-        file: pkg.browser,
-        format: 'iife',
-        sourcemap: 'inline',
-        banner,
-      },
-      // Minified
-      {
-        name: browserVar,
-        file: pkg.browser.replace('.js', '.min.js'),
-        format: 'iife',
-        sourcemap: 'inline',
-        banner,
-        plugins: [pluginTerser()],
-      },
-    ],
-    plugins: [
-      pluginTypescript(),
-      pluginCommonjs({
-        extensions: ['.js', '.ts'],
-      }),
-      pluginBabel({
-        babelHelpers: 'bundled',
-        configFile: path.resolve(__dirname, '.babelrc.js'),
-      }),
-      pluginNodeResolve({
-        browser: true,
-      }),
-    ],
-  },
-]
+export default {
+  input: inputFileName,
+  output: [
+    {
+      name: browserVar,
+      file: pkg.main,
+      format: 'umd',
+      sourcemap: 'inline',
+      banner,
+      exports: 'default',
+      globals: {},
+    },
+    {
+      name: browserVar,
+      file: pkg.main.replace('.js', '.min.js'),
+      format: 'umd',
+      sourcemap: 'inline',
+      banner,
+      exports: 'default',
+      globals: {},
+      plugins: [pluginTerser()],
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {})],
+  plugins: [
+    pluginTypescript(),
+    pluginCommonjs({
+      extensions: ['.js', '.ts'],
+    }),
+    pluginBabel({
+      babelHelpers: 'bundled',
+      configFile: path.resolve(__dirname, '.babelrc.js'),
+    }),
+    pluginNodeResolve({
+      browser: false,
+    }),
+  ],
+}
